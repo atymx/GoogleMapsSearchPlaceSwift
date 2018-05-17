1aeeda781e0ae0ac41068ce586fe208cbaf372a3
Maksim <macsim.1999@gmail.com>
2018-05-18 02:04:35 +0300

    Load google map with current location

---
+++ GoogleMapsSearchPlaceSwift/Config.swift
@@ -0,0 +1,14 @@
+//
+//  Config.swift
+//  GoogleMapsSearchPlaceSwift
+//
+//  Created by Максим Атюцкий on 18.05.2018.
+//  Copyright © 2018 Максим Атюцкий. All rights reserved.
+//
+
+import Foundation
+
+class Config {
+    static let shared = Config()
+    var GoogleMapsApiKey = "AIzaSyAzhnMBhkMtXBkRl2V3C9VAx4Z7-2xDa10"
+}
--- GoogleMapsSearchPlaceSwift/ViewController.swift
+++ GoogleMapsSearchPlaceSwift/ViewController.swift
@@ -7,19 +7,40 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UISearchDisplayDelegate {
+    
+    @IBOutlet weak var SearchBar: UISearchBar!
+    
+    @IBOutlet weak var MapView: GMSMapView!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.locationManager.requestWhenInUseAuthorization()
+        let (latitude, longitude) = (self.locationManager.location?.coordinate.latitude, self.locationManager.location?.coordinate.latitude)
+        self.LoadMap(latitude: latitude!, longitude: longitude!, title: "You are here")
     }
-
+    
+    func LoadMap(latitude: Double, longitude: Double, title: String) {
+        // Create a GMSCameraPosition that tells the map to display the
+        let camera = GMSCameraPosition.camera(withLatitude: latitude, longitude: longitude, zoom: 6.0)
+        MapView.isMyLocationEnabled = true
+        MapView.camera = camera
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        marker.title = title
+        marker.map = MapView
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
